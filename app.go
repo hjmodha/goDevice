9ad937057799a3173b1ef46e2cdfe682cdc0a855
Hitesh Modha <hiteshmodha10@gmail.com>
2015-02-17 17:24:15 +0530

    changes

--- app.go
+++ app.go
@@ -1,26 +1,20 @@
 package main
 
 import (
-  "html/template"
-  "log"
+  "fmt"
   "net/http"
-  "path"
 )
 
-func main() {
-  fs := http.FileServer(http.Dir("static"))
-  http.Handle("/static/", http.StripPrefix("/static/", fs))
-
-  http.HandleFunc("/", serveTemplate)
-
-  log.Println("Listening...")
-  http.ListenAndServe(":3000", nil)
+func handler(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello world!")
 }
 
-func serveTemplate(w http.ResponseWriter, r *http.Request) {
-  lp := path.Join("templates", "layout.html")
-  fp := path.Join("templates", r.URL.Path)
+func home(w http.ResponseWriter,r *http.Request){
+  fmt.Fprintf(w,"Home Page")
+}
 
-  tmpl, _ := template.ParseFiles(lp, fp)
-  tmpl.ExecuteTemplate(w, "layout", nil)
+func main() {
+  http.HandleFunc("/", handler)
+  http.HandleFunc("/home", home)
+  http.ListenAndServe(":3000", nil)
 }
