04448cca5055eb5e51959d03387abe07eb743f35
Hitesh Modha <hiteshmodha10@gmail.com>
2015-02-21 14:42:20 +0530

    detected device

--- app.go
+++ app.go
@@ -4,28 +4,32 @@ import (
   "fmt"
   "net/http"
   "time"
+  "strings"
 )
 
 func RouteHandler(w http.ResponseWriter, r *http.Request) {
   start := time.Now()
+  userAgent := r.Header.Get("User-Agent")
 
-  http.ServeFile(w, r, "html/index.html")
+  isMobile := strings.Contains(userAgent,"Android") ||
+      strings.Contains(userAgent,"webOS") ||
+      strings.Contains(userAgent,"iPad") ||
+      strings.Contains(userAgent,"iPhone") ||
+      strings.Contains(userAgent,"iPod") ||
+      strings.Contains(userAgent,"BlackBerry") ||
+      strings.Contains(userAgent,"Windows Phone")
+
+  if isMobile {
+    fmt.Fprintf(w,"<h1>Mobile</h1>")
+  }else{
+    fmt.Fprintf(w,"<h1>Web</h1>")
+  }
 
   elapsed := time.Since(start)
   fmt.Printf("Time take to serve static file %s\n\n", elapsed)
 }
 
-func home(w http.ResponseWriter,r *http.Request){
-  http.ServeFile(w, r, "html/index.html")
-}
-
-func notfound(w http.ResponseWriter,r *http.Request){
-  fmt.Fprintf(w,"Page Not found")
-}
-
 func main() {
-
   http.HandleFunc("/", RouteHandler)
-
   http.ListenAndServe(":3000", nil)
 }
