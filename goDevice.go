d6e878e3a6456bae43a0e55b61aaa6abf2e1454e
Jan Berktold <janberktold@aol.com>
2015-02-24 18:40:20 +0100

    refactored code

--- goDevice.go
+++ goDevice.go
@@ -1,43 +1,54 @@
 package goDevice
 
 import (
-  "net/http"
-  "strings"
+	"net/http"
+	"strings"
 )
 
-func IsMobile(r *http.Request) string {
-    userAgent := r.Header.Get("User-Agent")
-    deviceType := "Web";
+var Database = map[string][]string{
+	"Mobile": []string{
+		"Android",
+		"webOS",
+		"iPhone",
+		"BlackBerry",
+		"Windows Phone",
+	},
+	"Tab": []string{
+		"iPad",
+		"iPod",
+		"tablet",
+		"RX-34",
+		"FOLIO",
+		"Kindle",
+		"Mac OS",
+		"Silk",
+		"AppleWebKit",
+	},
+	"TV": []string{
+		"TV",
+		"GoogleTV",
+		"SmartTV",
+		"Internet TV",
+		"NetCast",
+		"NETTV",
+		"AppleTV",
+		"boxee",
+		"Kylo",
+		"Roku",
+		"DLNADOC",
+	},
+}
 
-    if strings.Contains(userAgent,"Android") ||
-        strings.Contains(userAgent,"webOS") ||
-        strings.Contains(userAgent,"iPhone") ||
-        strings.Contains(userAgent,"BlackBerry") ||
-        strings.Contains(userAgent,"Windows Phone") {
-          deviceType = "Mobile"
-    }else if strings.Contains(userAgent,"iPad") ||
-        strings.Contains(userAgent,"iPod") ||
-        (strings.Contains(userAgent,"tablet") ||
-        strings.Contains(userAgent,"RX-34") ||
-        strings.Contains(userAgent,"FOLIO")) ||
-        (strings.Contains(userAgent,"Kindle") ||
-        strings.Contains(userAgent,"Mac OS") &&
-        strings.Contains(userAgent,"Silk")) ||
-        (strings.Contains(userAgent,"AppleWebKit") &&
-        strings.Contains(userAgent,"Silk")){
-        deviceType = "Tab"
-    }else if strings.Contains(userAgent,"TV") ||
-      strings.Contains(userAgent,"GoogleTV") ||
-      strings.Contains(userAgent,"SmartTV") ||
-      strings.Contains(userAgent,"Internet TV") ||
-      strings.Contains(userAgent,"NetCast") ||
-      strings.Contains(userAgent,"NETTV") ||
-      strings.Contains(userAgent,"AppleTV") ||
-      strings.Contains(userAgent,"boxee") ||
-      strings.Contains(userAgent,"Kylo") ||
-      strings.Contains(userAgent,"Roku") ||
-      strings.Contains(userAgent,"DLNADOC"){
-        deviceType="TV"
-      }
-  return deviceType
+func IsMobile(r *http.Request) string {
+	userAgent := r.Header.Get("User-Agent")
+
+	for typ, list := range Database {
+		for _, extract := range list {
+			if strings.Contains(userAgent, extract) {
+				return typ
+			}
+		}
+	}
+
+	return "web"
 }
